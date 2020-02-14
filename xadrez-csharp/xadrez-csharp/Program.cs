23fb09898079efcabc025c5f84583d446b61d197
Andre B Anjos <aanjos.brandemburg@gmail.com>
2020-02-14 11:46:36 -0200

    Created Position class

--- xadrez-csharp/xadrez-csharp/Program.cs
+++ xadrez-csharp/xadrez-csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using board;
 
 namespace xadrez_csharp
 {
@@ -6,7 +7,12 @@ namespace xadrez_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+            Console.ReadLine();
         }
     }
 }
