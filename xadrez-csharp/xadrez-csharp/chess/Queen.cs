565264f01a7c2b43e6aefb2f87c1d342b681df99
Andre B Anjos <aanjos.brandemburg@gmail.com>
2020-03-02 16:08:57 -0300

    Added queen class and move logic

---
+++ xadrez-csharp/xadrez-csharp/chess/Queen.cs
@@ -0,0 +1,121 @@
+﻿using board;
+
+namespace chess
+{
+  class Queen : Piece
+  {
+
+    public Queen (Board board, Color color) : base (board, color)
+    {
+
+    }
+
+    public override string ToString ()
+    {
+      return "Q";
+    }
+
+    private bool CanMove (Position pos)
+    {
+      Piece p = Board.Piece(pos);
+      return p == null || p.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves ()
+    {
+      bool[,] mat = new bool[Board.Lines, Board.Colunms];
+      Position pos = new Position(0, 0);
+
+      pos.DefineValues(Position.Line + 1, Position.Colunm);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null || Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.Line = pos.Line + 1;
+      }
+
+      pos.DefineValues(Position.Line - 1, Position.Colunm);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null || Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.Line = pos.Line - 1;
+      }
+
+      pos.DefineValues(Position.Line, Position.Colunm + 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null || Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.Colunm = pos.Colunm + 1;
+      }
+
+      pos.DefineValues(Position.Line, Position.Colunm - 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null || Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.Line = pos.Line - 1;
+      }
+
+      pos.DefineValues(Position.Line - 1, Position.Colunm - 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null && Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+
+        pos.DefineValues(pos.Line - 1, pos.Colunm - 1);
+      }
+
+      pos.DefineValues(Position.Line + 1, Position.Colunm + 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null && Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.DefineValues(pos.Line + 1, pos.Colunm + 1);
+      }
+
+      pos.DefineValues(Position.Line - 1, Position.Colunm + 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null && Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.DefineValues(pos.Line - 1, pos.Colunm + 1);
+      }
+
+      pos.DefineValues(Position.Line + 1, Position.Colunm - 1);
+      while (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+        if (Board.Piece(pos) != null && Board.Piece(pos).Color != Color)
+        {
+          break;
+        }
+        pos.DefineValues(pos.Line + 1, pos.Colunm - 1);
+      }
+
+      return mat;
+    }
+  }
+}
