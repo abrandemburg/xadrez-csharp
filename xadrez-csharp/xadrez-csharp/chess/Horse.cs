be7c1e7f7160bd7c7b0b68da9bcedf206a986c8e
Andre B Anjos <aanjos.brandemburg@gmail.com>
2020-03-02 16:08:23 -0300

    added horse class and move logic

---
+++ xadrez-csharp/xadrez-csharp/chess/Horse.cs
@@ -0,0 +1,80 @@
+﻿using board;
+
+namespace chess
+{
+  class Horse : Piece
+  {
+
+    public Horse (Board board, Color color) : base(board, color)
+    {
+
+    }
+
+    public override string ToString ()
+    {
+      return "H";
+    }
+
+    private bool CanMove (Position pos)
+    {
+      Piece p = Board.Piece(pos);
+      return p == null || p.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves ()
+    {
+      bool[,] mat = new bool[Board.Lines, Board.Colunms];
+      Position pos = new Position(0, 0);
+
+      pos.DefineValues(Position.Line - 1, Position.Colunm - 2);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line - 1, Position.Colunm + 2);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line + 1, Position.Colunm - 2);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line + 1, Position.Colunm + 2);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line + 2, Position.Colunm + 1);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line + 2, Position.Colunm - 1);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line - 2, Position.Colunm + 1);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      pos.DefineValues(Position.Line - 2, Position.Colunm - 1);
+      if (Board.ValidPosition(pos) && CanMove(pos))
+      {
+        mat[pos.Line, pos.Colunm] = true;
+      }
+
+      return mat;
+    }
+  }
+}
